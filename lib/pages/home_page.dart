fcba808abec9d4940c0bda112cb75f3aa7bb521b
Yatharth Dhingra <yatharthdhingra1@gmail.com>
2022-03-24 17:21:50 +0530

    Added background screen animation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   double _buttonRadius = 100; //initially
 
+  //basically will go from 0% to 100%
+  final Tween<double> _backgroundScale = Tween<double>(begin: 0.0, end: 1.0);
+  //a tween is also used for animation
+  //this tween is of double data type as its start and end value are of type double
+  //tween maps all the values between this start and end and go over them
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,9 +33,26 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _pageBackground() {
-    return Container(
-      color: Colors.blue,
-    ); //for background color
+    return TweenAnimationBuilder(
+      tween: _backgroundScale,
+      duration: const Duration(seconds: 2),
+
+      curve:
+          Curves.easeInOutCubicEmphasized, //we can also add curves to this too
+
+      //a function that returns the animation
+      builder: (_context, double _scale, _child) {
+        // _scale is the value of the tween at a specific moment , _child is the child on which we are operating the animation on
+        return Transform.scale(
+          //tranform is linearly moving with time
+          scale: _scale,
+          child: _child,
+        );
+      },
+      child: Container(
+        color: Colors.blue,
+      ),
+    );
   }
 
   Widget _circularAnimationButton() {
@@ -49,19 +72,25 @@ class _HomePageState extends State<HomePage> {
 
               duration: const Duration(
                   seconds: 2), //duration should be given to the animation
-                  
-              curve: Curves.bounceIn,   //our animation takes place linearly by default 
+
+              curve: Curves
+                  .bounceIn, //our animation takes place linearly by default
               //if we want to change how it transitions from start to end , we do it by using curve
 
               height:
                   _buttonRadius, //did both _deviceWidth as it should be a circle
               width: _buttonRadius,
               decoration: BoxDecoration(
-                  color: Colors.purple,  
+                  color: Colors.purple,
                   borderRadius: BorderRadius.circular(1000)),
-                  // we gave such a high radius that it becomes circular
+              // we gave such a high radius that it becomes circular
 
-              child: const  Center(child: Text("Button!" , style: TextStyle(color: Colors.white),),),
+              child: const Center(
+                child: Text(
+                  "Button!",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
               //a centered text inside the button
             )));
   }
